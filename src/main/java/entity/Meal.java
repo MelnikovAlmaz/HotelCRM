9aba527b7f456370f9fc8fee6603ee2d1a98e25d
MelnikovAlmaz <melnikovalmaz@gmail.com>
2016-11-08 14:19:12 +0300

    Create entity Menu and Meal

---
+++ src/main/java/entity/Meal.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class Meal {
+    private Integer id;
+    private String name;
+    private Double price;
+    private Menu menu;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+}
---
+++ src/main/java/entity/Menu.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Menu {
+    private Integer id;
+    private Hotel hotel;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+}
