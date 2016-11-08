a1fff3a55a11447abc9464c610273135c8df64f2
MelnikovAlmaz <melnikovalmaz@gmail.com>
2016-11-08 13:45:28 +0300

    Create entity Guest

---
+++ src/main/java/entity/Guest.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class Guest {
+    private Integer id;
+    private String name;
+    private String phoneNumber;
+    private String passport;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+}
