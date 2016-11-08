e7421e8c51a52b6ca0ab5d1c67ee4bb03726e5e8
MelnikovAlmaz <melnikovalmaz@gmail.com>
2016-11-08 19:59:12 +0300

    Refactor HotelMapper

--- src/main/java/repository/mapper/HotelMapper.java
+++ src/main/java/repository/mapper/HotelMapper.java
@@ -1,12 +1,21 @@
 package repository.mapper;
 
+import entity.City;
+import entity.Employee;
 import entity.Hotel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import repository.CityRepository;
+import repository.EmployeeRepository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class HotelMapper implements RowMapper<Hotel> {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    CityRepository cityRepository;
 
     @Override
     public Hotel mapRow(ResultSet resultSet, int i) throws SQLException {
@@ -16,6 +25,10 @@ public class HotelMapper implements RowMapper<Hotel> {
         hotel.setAddress(resultSet.getString("address"));
         hotel.setPhoneNumber(resultSet.getString("phoneNumber"));
         hotel.setDescription(resultSet.getString("description"));
+        Employee manager = employeeRepository.findEmployeeById(resultSet.getInt("managerId"));
+        hotel.setManager(manager);
+        City city = cityRepository.findCityById(resultSet.getInt("cityId"));
+        hotel.setCity(city);
         return hotel;
     }
 }
